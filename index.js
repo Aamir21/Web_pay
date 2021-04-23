d8406abb7dbf716dd88a705b244d7a439daf89fe
Md. Aamir Sohail <42406623+Aamir21@users.noreply.github.com>
2021-04-23 16:59:50 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,9 @@
+var app = angular.module('myApp', []);
+app.controller('validateCtrl', function($scope) {
+ // $scope.firstname = 'John Doe';
+  //$scope.email = 'john.doe@gmail.com';
+});
+
+
+toastr.info('Are you the 6 fingered man?');
+
